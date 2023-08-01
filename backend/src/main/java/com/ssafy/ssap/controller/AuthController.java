29dc60934298dad528c732111adf6460a9ed289a
김소이 <thdl9893@ajou.ac.kr>
2023-08-01 17:25:39 +0900

    Feat: 로그인 시 응답 데이터 수정

--- backend/src/main/java/com/ssafy/ssap/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/ssap/controller/AuthController.java
@@ -21,10 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ssap.common.MessageFormat;
 import com.ssafy.ssap.dto.TokenDto;
-import com.ssafy.ssap.dto.user.LoginDto;
+import com.ssafy.ssap.dto.user.LoginRequestDto;
+import com.ssafy.ssap.dto.user.LoginResponseDto;
 import com.ssafy.ssap.jwt.JwtFilter;
 import com.ssafy.ssap.jwt.TokenProvider;
 import com.ssafy.ssap.service.EmailService;
+import com.ssafy.ssap.service.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -36,20 +38,39 @@ public class AuthController {
 	private static final Logger logger = LoggerFactory.getLogger(RoomController.class);
 	private final TokenProvider tokenProvider;
 	private final AuthenticationManagerBuilder authenticationManagerBuilder;
-	// private final UserService userService;
+	private final UserService userService;
 	private final EmailService emailService;
 
 	@PostMapping("/login")
-	public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
-		UsernamePasswordAuthenticationToken authenticationToken =
-			new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
-		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = tokenProvider.createToken(authentication);
-		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+	public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
 
-		return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+		try {
+			UsernamePasswordAuthenticationToken authenticationToken =
+				new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+			Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			String jwt = tokenProvider.createToken(authentication);
+
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+			LoginResponseDto loginResponseDto = userService.getUserWithEmail(loginRequestDto.getEmail());
+
+			resultMap.put("message", MessageFormat.SUCCESS);
+			resultMap.put("user", loginResponseDto);
+			resultMap.put("jwt", new TokenDto(jwt));
+
+			status = HttpStatus.ACCEPTED;
+			return new ResponseEntity<>(resultMap, httpHeaders, status);
+		} catch (Exception e) {
+			logger.error("로그인 실패: ", e);
+			resultMap.put("message", MessageFormat.SERVER_FAIL + ": " + e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 
 	@GetMapping("/join/{email}")
@@ -58,10 +79,9 @@ public class AuthController {
 		HttpStatus status = null;
 
 		try {
-			if (emailService.sendEmail(email).equals("false")) {
+			if (!emailService.sendEmail(email)) {
 				logger.debug("{}이미 존재하는 사용자", email);
 				resultMap.put("message", "Already exists");
-				// Return a 409 Conflict response if the user already exists
 				status = HttpStatus.CONFLICT;
 			} else {
 				logger.debug("{} 인증번호 전송 성공", email);
