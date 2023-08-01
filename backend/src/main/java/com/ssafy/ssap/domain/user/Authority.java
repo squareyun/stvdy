1e8cfad6c3a8f24a73886a622c0045c05f325997
김소이 <thdl9893@ajou.ac.kr>
2023-08-01 14:35:51 +0900

    Style: 코드 정리 및 설명 주석 추가

--- backend/src/main/java/com/ssafy/ssap/domain/user/Authority.java
+++ backend/src/main/java/com/ssafy/ssap/domain/user/Authority.java
@@ -1,11 +1,14 @@
 package com.ssafy.ssap.domain.user;
 
-import lombok.*;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "authority")
@@ -15,7 +18,7 @@ import jakarta.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Authority {
-    @Id
-    @Column(name = "authority_name", length = 50)
-    private String authorityName;
+	@Id
+	@Column(name = "authority_name", length = 50)
+	private String authorityName;
 }
--- backend/src/main/java/com/ssafy/ssap/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/ssafy/ssap/service/CustomUserDetailsService.java
@@ -1,9 +1,8 @@
 package com.ssafy.ssap.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
-import com.ssafy.ssap.domain.user.User;
-
-import com.ssafy.ssap.repository.UserRepository;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,37 +11,36 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.ssafy.ssap.domain.user.User;
+import com.ssafy.ssap.repository.UserRepository;
 
 @Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	public CustomUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(final String email) {
-        return userRepository.findOneWithAuthoritiesByEmail(email)
-                .map(user -> createUser(email, user))
-                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
-    }
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(final String email) {
+		return userRepository.findOneWithAuthoritiesByEmail(email)
+			.map(user -> createUser(email, user))
+			.orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
+	}
 
-    private org.springframework.security.core.userdetails.User createUser(String username, User user) {
-        if (!user.isActivated()) {
-            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
-        }
+	private org.springframework.security.core.userdetails.User createUser(String username, User user) {
+		if (!user.isActivated()) {
+			throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+		}
 
-        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
-                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
-                .collect(Collectors.toList());
+		List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+			.map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+			.collect(Collectors.toList());
 
-        return new org.springframework.security.core.userdetails.User(user.getEmail(),
-                user.getPassword(),
-                grantedAuthorities);
-    }
+		return new org.springframework.security.core.userdetails.User(user.getEmail(),
+			user.getPassword(), grantedAuthorities);
+	}
 }
 
