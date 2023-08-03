b3cfbdc50a125315e969db519ffe68e5baa23ecc
김소이 <thdl9893@ajou.ac.kr>
2023-08-03 17:58:22 +0900

    Feat: 유저 Id 타입 변경

--- backend/src/main/java/com/ssafy/ssap/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/ssap/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ssap.domain.user.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 	@EntityGraph(attributePaths = "authorities")
 	Optional<User> findOneWithAuthoritiesByEmail(String email);
