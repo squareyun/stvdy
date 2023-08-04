861825a33958c1aa6768d2dbfd06108194d54814
김소이 <thdl9893@ajou.ac.kr>
2023-08-04 10:01:52 +0900

    Fix: Participant 테이블 참조키 관련 에러 수정

--- backend/src/main/java/com/ssafy/ssap/domain/studyroom/Participant.java
+++ backend/src/main/java/com/ssafy/ssap/domain/studyroom/Participant.java
@@ -1,16 +1,25 @@
 package com.ssafy.ssap.domain.studyroom;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import static jakarta.persistence.FetchType.*;
+
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import static jakarta.persistence.FetchType.LAZY;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@SuppressWarnings("checkstyle:RegexpSinglelineJava")
 @Entity
 @Getter
 @NoArgsConstructor
@@ -18,20 +27,21 @@ import static jakarta.persistence.FetchType.LAZY;
 @AllArgsConstructor
 public class Participant {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(name = "is_out", columnDefinition = "bit")
-    @ColumnDefault("0")
-    private Boolean isOut;
+	@Column(name = "is_out", columnDefinition = "bit")
+	@ColumnDefault("0")
+	private Boolean isOut;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "role")
-    private ParticipantRoleNs role;
+	@NotNull
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "role")
+	private ParticipantRoleNs role;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "room_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Room room;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "room_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Room room;
 }
--- backend/src/main/java/com/ssafy/ssap/domain/studyroom/ParticipantRoleNs.java
+++ backend/src/main/java/com/ssafy/ssap/domain/studyroom/ParticipantRoleNs.java
@@ -3,19 +3,19 @@ package com.ssafy.ssap.domain.studyroom;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "participants_role_ns")
 @AllArgsConstructor
 @NoArgsConstructor
 public class ParticipantRoleNs {
 
-    @Id
-    @Column(length = 10)
-    @NotNull
-    private String name;
+	@Id
+	@Column(length = 10)
+	private String name;
 }
