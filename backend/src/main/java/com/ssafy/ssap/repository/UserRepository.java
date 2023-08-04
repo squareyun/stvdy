4223d734bb89d598ad4f01bb1125ec1ecd58e06f
김소이 <thdl9893@ajou.ac.kr>
2023-08-04 10:05:05 +0900

    Feat: 회원탈퇴 구현

--- backend/src/main/java/com/ssafy/ssap/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/ssap/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.ssafy.ssap.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ssap.domain.user.User;
+import com.ssafy.ssap.domain.user.UserStateNs;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
@@ -13,5 +15,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	Optional<User> findOneWithAuthoritiesByEmail(String email);
 
 	User findByEmail(String email);
+
+	List<User> findByStateAndActivated(UserStateNs state, boolean activated);
 
 }
