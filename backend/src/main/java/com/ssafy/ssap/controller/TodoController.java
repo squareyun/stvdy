c2f328c018994173999e43c0981b1f09c12e9eea
김소이 <thdl9893@ajou.ac.kr>
2023-08-07 14:58:40 +0900

    Feat: todo 완료 표시(done_flag 토글) 구현

--- backend/src/main/java/com/ssafy/ssap/controller/TodoController.java
+++ backend/src/main/java/com/ssafy/ssap/controller/TodoController.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,11 +33,29 @@ public class TodoController {
 		HttpStatus status = null;
 		try {
 			Integer todoId = todoService.create(map.get("objective"));
-			logger.debug("{} todo 생성 성공", todoId);
+			logger.info("{} todo 생성 성공", todoId);
 			resultMap.put("message", MessageFormat.SUCCESS);
 			status = HttpStatus.ACCEPTED;
 		} catch (Exception e) {
 			logger.error("todo 생성 실패: {}", e.getMessage());
+			resultMap.put("message", MessageFormat.SERVER_FAIL + ": " + e.getClass().getSimpleName());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+
+	@PutMapping("/{todoId}")
+	public ResponseEntity<Map<String, Object>> add(@PathVariable Integer todoId) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			todoService.checkDoneFlag(todoId);
+			logger.info("{} todo toggle 표시 성공", todoId);
+			resultMap.put("message", MessageFormat.SUCCESS);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			logger.error("todo toggle 표시 실패: {}", e.getMessage());
 			resultMap.put("message", MessageFormat.SERVER_FAIL + ": " + e.getClass().getSimpleName());
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
--- backend/src/main/java/com/ssafy/ssap/service/TodoService.java
+++ backend/src/main/java/com/ssafy/ssap/service/TodoService.java
@@ -39,4 +39,21 @@ public class TodoService {
 		return todo.getId();
 	}
 
+	public void checkDoneFlag(Integer todoId) {
+		String userEmail = userService.getMyUserWithAuthorities().getEmail();
+		User user = userRepository.findOneWithAuthoritiesByEmail(userEmail)
+			.orElseThrow(() -> new IllegalArgumentException("사용자 정보를 찾을 수 없습니다."));
+		Todo todo = todoRepository.findById(todoId)
+			.orElseThrow(() -> new IllegalArgumentException("todo 정보를 찾을 수 없습니다."));
+		;
+		Boolean flag = todo.getDoneFlag();
+
+		if (user.getId() != todo.getUser().getId()) {
+			new IllegalArgumentException("사용자 정보가 일치하지 않습니다.");
+		}
+
+		todo.setDoneFlag(!flag);
+		todoRepository.save(todo);
+	}
+
 }
