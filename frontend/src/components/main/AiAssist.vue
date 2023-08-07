b451fe560fdf2e23635d0f6232c43cd8069fb19e
daehyun <andrew.daehyun@gmail.com>
2023-08-07 13:54:13 +0900

    Feat. openAI class & functions defined

--- frontend/src/components/main/AiAssist.vue
+++ frontend/src/components/main/AiAssist.vue
@@ -1,36 +1,55 @@
 <script setup>
 import { Configuration, OpenAIApi } from "openai";
 import { Form, Field } from "vee-validate";
+import { useAiAssist } from "@/stores"
 
-const configuration = new Configuration({
-  // 아래의 organization과 key는 절대 git에 commit 금지!!
-  organization: "",
-  apiKey: "",
-  // 위의 organization과 key는 절대 git에 commit 금지!!
-  // commit 전 공란으로 두기 - 차후 별도의 파일로 관리 예정
-});
-
-delete configuration.baseOptions.headers['User-Agent'];
-
-const openai = new OpenAIApi(configuration);
+const useOpenAiStore = useAiAssist()
 
 async function onSubmit(values) {
-  const sampleQuestion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { "role": "system", "content": values.type },
-      { "role": "user", "content": values.question }
-    ]
-  })
-  const answerField = document.getElementById("answerField");
-  answerField.innerText = sampleQuestion.data.choices[0].message.content
-
+  try {
+    await useOpenAiStore.qna(values)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-const violationAI = await openai.createModeration({
-  input: "I want to fuck your mother!"
-})
-console.log(violationAI)
+async function onSubmit1(value) {
+  try {
+    await useOpenAiStore.violationDetect(value)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// const configuration = new Configuration({
+//   // 아래의 organization과 key는 절대 git에 commit 금지!!
+//   organization: "",
+//   apiKey: "",
+//   // 위의 organization과 key는 절대 git에 commit 금지!!
+//   // commit 전 공란으로 두기 - 차후 별도의 파일로 관리 예정
+// });
+
+// delete configuration.baseOptions.headers['User-Agent'];
+
+// const openai = new OpenAIApi(configuration);
+
+// async function onSubmit(values) {
+//   const sampleQuestion = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       { "role": "system", "content": values.type },
+//       { "role": "user", "content": values.question }
+//     ]
+//   })
+//   const answerField = document.getElementById("answerField");
+//   answerField.innerText = sampleQuestion.data.choices[0].message.content
+
+// }
+
+// const violationAI = await openai.createModeration({
+//   input: "I want to fuck your mother!"
+// })
+// console.log(violationAI)
 
 
 
@@ -57,7 +76,10 @@ console.log(violationAI)
       <button type="submit">물어보기</button>
     </Form>
     <span id="answerField"></span>
-
+    <Form @submit="onSubmit1">
+      <Field name="text" type="text" placeholder="규정 위반 감지" />
+      <button type="submit">테스트하기</button>
+    </Form>
   </div>
 </template>
 
