e386d0ef46333a4c77537848a235d4b772bed102
he2kape <he2kape@gmail.com>
2023-08-13 21:44:51 +0900

    Feat: 화상방 내부 프론트

--- frontend/src/components/webrtc/UserVideo.vue
+++ frontend/src/components/webrtc/UserVideo.vue
@@ -1,37 +1,65 @@
+<script>
+export default {
+  name: 'UserVideo',
+}
+</script>
+
+<script setup>
+// import { ref, onMounted, computed } from 'vue';
+import { computed } from 'vue'
+import OvVideo from '@/components/webrtc/OvVideo.vue'
+
+const props = defineProps({
+  streamManager: Object,
+})
+
+// clientData는 computed로 진행됨
+const clientData = computed(() => {
+  const { clientData } = getConnectionData()
+  return clientData
+})
+
+function getConnectionData() {
+  const { connection } = props.streamManager.stream
+  return JSON.parse(connection.data)
+}
+</script>
+
 <template>
-  <div v-if="streamManager">
-    <OvVideo :stream-manager="streamManager" />
+  <div
+    class="user-box"
+    v-if="streamManager">
+    <OvVideo
+      class="video-box"
+      :stream-manager="streamManager" />
     <!-- {{ clientData }}이게 내 현재 이름임 -->
-    <div>
+    <div class="video-user-name">
       <p>{{ clientData }}</p>
     </div>
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'UserVideo',
-  }
-</script>
+<style>
+.user-box {
+  position: relative;
+  width: calc(33% - 4px);
+  height: calc(33% - 4px);
 
-<script setup>
-  // import { ref, onMounted, computed } from 'vue';
-  import { computed } from 'vue';
-  import OvVideo from '@/components/webrtc/OvVideo.vue';
+  padding: 2px 2px 2px 2px;
+}
 
+.video-box {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+}
 
-  const props = defineProps({
-    streamManager: Object,
-  })
+.video-user-name {
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
 
-  // clientData는 computed로 진행됨
-  const clientData = computed(() => {
-    const { clientData } = getConnectionData();
-    return clientData;
-  });
-
-  function getConnectionData() {
-    const { connection } = props.streamManager.stream;
-    return JSON.parse(connection.data);
-  }
-</script>
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+</style>
