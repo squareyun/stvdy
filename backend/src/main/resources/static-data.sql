a81785a2fc3b3ed1a74732f3043f638785f15d17
윤우혁 <squareyun@gmail.com>
2023-08-16 09:27:42 +0900

    hotfix: sql 수정

--- backend/src/main/resources/static-data.sql
+++ backend/src/main/resources/static-data.sql
@@ -6,14 +6,14 @@ INSERT INTO participants_role_ns (name) VALUES ('호스트'), ('스태프'), ('�
 INSERT INTO authority (authority_name) VALUES ('ROLE_USER');
 INSERT INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 
-INSERT INTO user (email, name, password, nickname, activated, state, regist_time) VALUES ('admin@email.com', 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1, '사용자', now());
-INSERT INTO user (email, name, password, nickname, activated, state, regist_time) VALUES ('user@email.com', 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1, '사용자', now());
+-- INSERT INTO user (email, name, password, nickname, activated, state, regist_time) VALUES ('admin@email.com', 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1, '사용자', now());
+-- INSERT INTO user (email, name, password, nickname, activated, state, regist_time) VALUES ('user@email.com', 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1, '사용자', now());
 
 INSERT INTO interest (user_id, tag) VALUES (2, '자격증'), (2, '수능');
 
-INSERT INTO user_authority (user_no, authority_name) VALUES (1, 'ROLE_USER');
-INSERT INTO user_authority (user_no, authority_name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO user_authority (user_no, authority_name) VALUES (2, 'ROLE_USER');
+-- INSERT INTO user_authority (user_no, authority_name) VALUES (1, 'ROLE_USER');
+-- INSERT INTO user_authority (user_no, authority_name) VALUES (1, 'ROLE_ADMIN');
+-- INSERT INTO user_authority (user_no, authority_name) VALUES (2, 'ROLE_USER');
 
 --ALTER TABLE LIKES DROP FOREIGN KEY FK3p9ex2qw452lgox5sboky1e98;
 --ALTER TABLE LIKES DROP FOREIGN KEY FKnbhujedlj8a26j6mj51fkelve;
