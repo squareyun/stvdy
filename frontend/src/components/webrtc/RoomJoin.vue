b2faf412aff4005ec007ca72ca4a13c866eeea68
kimbeomgi <dyd1308@naver.com>
2023-08-21 09:45:36 +0900

    화질 변경

--- frontend/src/components/webrtc/RoomJoin.vue
+++ frontend/src/components/webrtc/RoomJoin.vue
@@ -365,8 +365,8 @@ function joinSession() {
           videoSource: undefined, // The source of video. If undefined default webcam
           publishAudio: !muted.value, // Whether you want to start publishing with your audio unmuted or not
           publishVideo: !cameraOff.value, // Whether you want to start publishing with your video enabled or not
-          resolution: '640x480', // The resolution of your video
-          // resolution: "1280x960", // The resolution of your video
+          // resolution: '640x480', // The resolution of your video
+          resolution: "280x210", // The resolution of your video  4:3
           frameRate: 30, // The frame rate of your video
           insertMode: 'APPEND', // How the video is inserted in the target element 'video-container'
           mirror: false, // Whether to mirror your local video or not
